0c2936c47f57158e1bcf07134885e584db8a9868
Mike Murray <50731334+MMMikeM@users.noreply.github.com>
2026-08-12 14:52:07 +0300

    test(index): pin each subpath entry's export surface
    The barrel test covered only the root entry; attw/publint validate
    resolution but not what each of ./client, ./send, ./vapid exports.

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,8 @@
 import { describe, expect, it } from "vite-plus/test";
+import * as clientEntry from "../src/client";
 import * as pkg from "../src/index";
+import * as sendEntry from "../src/send";
+import * as vapidEntry from "../src/vapid";
 
 describe("index barrel", () => {
 	const clientExports = [
@@ -28,5 +31,11 @@ describe("index barrel", () => {
 
 	it("exports exactly the expected public surface", () => {
 		expect(Object.keys(pkg).toSorted()).toEqual(expected.toSorted());
+	});
+
+	it("each subpath entry exports exactly its slice of that surface", () => {
+		expect(Object.keys(clientEntry).toSorted()).toEqual(clientExports.toSorted());
+		expect(Object.keys(sendEntry).toSorted()).toEqual(serverExports.toSorted());
+		expect(Object.keys(vapidEntry).toSorted()).toEqual(vapidExports.toSorted());
 	});
 });
